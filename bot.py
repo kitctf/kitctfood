ab6eadbbdfbeadd50bd3a84dd25781123d78b75a
Chrisliebär <11184804+chrisliebaer@users.noreply.github.com>
2025-03-07 01:20:51 +0100

    🚸 Quality of life update (#2)
    * Remove additional reactions once poll has concluded, so users no
    longer have to do math
    * Prefer to use nicknames and first name instead of account name

--- bot.py
+++ bot.py
@@ -456,7 +456,11 @@ class ActivePoll:
 						return
 
 					if post["root_id"] == self.checkpoint.post_id:
-						author = event["data"]["sender_name"]
+						# fetch user to get nickname
+						user = await self.bot.driver.users.get_user(post["user_id"])
+
+						# use nickname if available, otherwise "first_name", if both are missing, use "sender_name"
+						author = user.get("nickname", user.get("first_name", event["data"]["sender_name"]))
 						content = post["message"]
 						logging.debug(f"Received reply to poll message ({self.checkpoint.post_id}) from {author}: {content}")
 						await self.handle_poll_message(author, content)
@@ -575,6 +579,11 @@ class ActivePoll:
 			'message': final_message,
 			'has_reactions': True
 		})
+
+		# remote additional reactions, since they inflate actual participant count
+		for reaction in self.bot.config.poll.additional_reactions:
+			logging.debug(f"Removing reaction {reaction} from post {self.checkpoint.post_id}")
+			await self.bot.driver.reactions.delete_reaction(self.bot.driver.client.userid, self.checkpoint.post_id, reaction)
 
 		# post just the end message to the thread
 		await self.send_message({
